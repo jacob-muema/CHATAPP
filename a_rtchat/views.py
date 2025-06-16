1a346f36b65bb49c54e2ab2212e82bcf25f97afb
jacob-muema <jacobmuema02@gmail.com>
2025-06-16 04:11:11 +0300

    new

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import ChatGroup
+from .forms import ChatmessageCreateForm
 
-# Create your views here.
-def chat_view (request):
-    return render (request, 'a_rtchat/chat.html')
+@login_required
+def chat_view(request):
+    chat_group = get_object_or_404(ChatGroup, group_name="masyopnetinternshipGroup")
+    chat_messages = chat_group.chat_messages.all().order_by('created')[:30]  # Oldest at top
+    form = ChatmessageCreateForm()
+
+    if request.htmx:
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group_name = chat_group  # ⚠️ Match the field name exactly as in your model
+            message.save()
+            context = {
+                'message' : message,
+                'user' :request.user
+            }
+            return render(request, 'a_rtchat/partials/chat_message_p.html',context)
+
+    return render(request, 'a_rtchat/chat.html', {
+        'chat_messages': chat_messages,
+        'form': form,
+    })
